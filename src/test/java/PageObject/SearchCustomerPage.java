4ee63fe116e624dba4d131a61a2700fe58293077
HimanshuKr-1999 <kumarhim3108@gmail.com>
2025-09-10 20:37:48 +0530

    New Features added

--- src/test/java/PageObject/SearchCustomerPage.java
+++ src/test/java/PageObject/SearchCustomerPage.java
@@ -1,5 +1,63 @@
 package PageObject;
 
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SearchCustomerPage {
 
+	
+	
+	WebDriver Ldriver;   //local Webdriver
+	public SearchCustomerPage(WebDriver Rdriver){ //remote webdriver
+		Ldriver=Rdriver;
+		
+		PageFactory.initElements(Rdriver, this);
+	}
+	
+	
+	
+	@FindBy(id="SearchEmail")
+	WebElement emailAdd;
+
+	@FindBy(id="search-customers")
+	WebElement searchBtn;
+
+	@FindBy(xpath="//table[@role='grid']")
+	WebElement searchResult;
+
+	@FindBy(xpath="//table[@id='customers-grid']//tbody/tr")
+	List<WebElement> tableRows;
+
+	/*@FindBy(xpath="//table[@id='customers-grid']//tbody/tr[1]/td")
+	List<WebElement> tableColumns;*/
+
+	@FindBy(id="SearchFirstName")
+	WebElement firstName;
+
+	@FindBy(id="SearchLastName")
+	WebElement lastName;
+
+	//action method to enter email address
+	public void enterEmailAdd(String email)
+	{
+		emailAdd.sendKeys(email);
+	}
+
+	//action method to perform click action on search button
+	public void clickOnSearchButton()
+	{
+		
+		searchBtn.click();
+		
+	}
+
+
+	
+	
+	
+	
 }
